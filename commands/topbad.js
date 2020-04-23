7e5c15fae62773109dd2e481fcda15c41ffb738f
Sen <hoangnhan26212@gmail.com>
2020-04-23 17:00:02 +0700

    Update topbad.js

--- commands/topbad.js
+++ commands/topbad.js
@@ -2,47 +2,41 @@ const Discord = require("discord.js");
 const botconfig = require("./botconfig.json");
 const bot = new Discord.Client();
 const fs = require('fs');
-var mongodb = require('mongodb');
-var MongoClient = mongodb.MongoClient;
-var url = 'mongodb+srv://sen:sen@data-2dbpw.gcp.mongodb.net/test';
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://roe:roe@roe-dewbn.azure.mongodb.net/data-ROE?retryWrites=true&w=majority',
+    { useUnifiedTopology: true ,useNewUrlParser: true}
+);
+const User = require('../models/user.js');
 bot.commands = new Discord.Collection();
 
 module.exports.run = async (__bot, message, args) => {
-  MongoClient.connect(url, function (err, db) {
-    if (err) {
-      console.log('Unable to connect to the mongoDB server. Error:', err);
-    } else {
-      //HURRAY!! We are connected. :)
-      console.log('Connection established to', url);
-    }
   
- var dbo = db.db("test");
+
  if(!message.member.hasPermission("ADMINISTRATOR")) return;
-var mysort = { nhacnho: -1 };
-  dbo.collection("ROE").find().sort(mysort).toArray(async function(err, result) {
-    if (err) throw err;
+
+
+ User.find({}).sort({nhacnho:-1}).exec(async function(err, result) { 
+   if(err) return message.channel.send("Đã xảy ra lỗi, vui lòng thử lại sau!");
     var punch = [
 
     ];
-    for(var i=0;i<20;i++){
-      await punch.push(`Hạng ${i+1}: <@!${result[i].id}> ${result[i].nhacnho} lần\n`);
-    if(19==i){
+    if(result.length<10)return message.channel.send("Đã xảy ra lỗi, vui lòng thử lại sau!");
+    for(var i=0;i<10;i++){
+      await punch.push(`Hạng ${i+1}: <@!${result[i].id}> ${result[i].nhacnho} tin nhắn\n`);
+    if(9==i){
       let hit = new Discord.RichEmbed()
         .setDescription(`${punch.join("")}`)
-        .setTitle("Top 20 người nói tục nhiều nhất")
+        .setTitle("Top 10 người nói tục nhiều nhất")
         .setColor("#15edf1")
         .setFooter("Code by Sen")
         .setTimestamp()
       return message.channel.send(hit);
     }
-    }
-    
-        
-  });    
-   
-   
+  } 
+  
+ });
+  
 
-});     
 }
 
 module.exports.help = {
