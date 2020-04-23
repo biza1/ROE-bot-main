57f8c3f79852d9c6798c0ac76f19e95db66df880
Sen <hoangnhan26212@gmail.con>
2020-04-23 14:57:16 +0700

    asd

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,4 @@
 const Discord = require("discord.js");
-// const bot = new Discord.Client();
-// bot.commands = new Discord.Collection();
 
 module.exports.run = async (bot, message, args) => {
     if(message.author.bot) return;
@@ -11,10 +9,7 @@ if(message.channel.id=="545269780005781506"){
       let check= message.member.hasPermission("ADMINISTRATOR")||message.member.roles.has("545275416232067072");
 if(!check) return message.channel.send("Vui lòng xuống <#545280256119013406> hoặc <#610833292312182830> để gọi lệnh Bot. Xin cảm ơn <:police:665946381101432881>");
 }
-<<<<<<< HEAD
 //Ping
-=======
->>>>>>> 8e03a8b3ca7d06a340789af973750dbc322bb3b6
 message.channel.send("yaaaaaaa....").then(m=>{
     let ping = m.createdTimestamp - message.createdTimestamp
     m.edit(`Latency: \`${ping} ms\``)
